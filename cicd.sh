c6742b9340b5364ac790d23fd80b0d71e383eed7
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2020-07-29 12:35:13 +1200

    Added initial working sonarqube scan and docker-compose sq instance

--- cicd.sh
+++ cicd.sh
@@ -1,9 +1,21 @@
+RUN_SONARQUBE=0
+
 
 echo "Starting build"
+
 DOCKER_IMAGE=demo-build-example
 # build
 docker build . -t "$DOCKER_IMAGE:build" --target build
 # test
+echo "Running $TEST_COMMAND";
 docker run --rm -v "$PWD/build_results:/results" "$DOCKER_IMAGE:build" dotnet test --collect:"XPlat Code Coverage" --logger:trx --results-directory:/results
+# analyse
+docker run \
+    --rm \
+    --network=host \
+    -e SONAR_HOST_URL="http://localhost:9000" \
+    -v "$PWD:/usr/src" \
+    sonarsource/sonar-scanner-cli -Dsonar.projectKey=myproject
+
 # publish
 echo "Going to publish now...Up up and away"
